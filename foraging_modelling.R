9436fb993e938510132358a0d25ac982130a12ca
francescorosa01 <francesco-rosadini@hotmail.it>
2026-03-18 16:33:04 +0100

    first model trials

---
+++ foraging_modelling.R
@@ -0,0 +1,39 @@
+# Load packages
+library(lme4)
+library(lmerTest)
+
+master <- read.csv(here("csv/processed/foraging_master.csv"))
+
+master$unique_trial_ID <- as.factor(master$unique_trial_ID)
+master$trial <- as.factor(master$trial)
+master$season <- as.factor(master$season)
+
+
+model1 <- lmer(first_island_time ~ trial + season + (1 | season_ID), data = master)
+summary(model1)
+# Residual plot
+plot(model1)
+# Normality of residuals
+qqnorm(resid(model1))
+qqline(resid(model1))
+
+# Total distance
+model_dist <- lmer(distance_rate ~ trial + season + (1 | season_ID), data = master)
+summary(model_dist)
+plot(model_dist)
+qqnorm(resid(model_dist))
+qqline(resid(model_dist))
+
+# Time moving
+model_move <- lmer(moving_time ~ trial + season + (1 | season_ID), data = master)
+summary(model_move)
+plot(model_move)
+qqnorm(resid(model_move))
+qqline(resid(model_move))
+
+#tot visits
+model_visit <- lmer(tot_visit ~ trial + season + (1 | season_ID), data = master)
+summary(model_visit)
+plot(model_visit)
+qqnorm(resid(model_visit))
+qqline(resid(model_visit))
